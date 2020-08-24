774541296ec186cbc023f10c6f7a4380f31c78c4
06swax <swax@DESKTOP-OH0P76I.localdomain>
2020-08-24 20:10:08 +0530

    initial commit

---
+++ wsl.sql
@@ -0,0 +1,3 @@
+create database portal_data;
+use portal_data; 
+create table [user_data] (personid int(50) not null auto_increment primary key,email varchar(50),firstname varchar(35),lastname varchar(50),password varchar(50));
